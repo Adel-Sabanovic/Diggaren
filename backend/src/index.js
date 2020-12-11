47481d8ece8c5c999d7c444199a02a4fc39c697b
yousef <yousefkhalil125@gmail.com>
2020-12-11 13:03:03 +0100

    initial setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,14 @@ import express from "express";
 
 import { rootRouter } from "./routes";
 
+import { json } from "body-parser"
+
 
 const app = express();
 
 const port = 8000;
+
+app.use(json());
 
 app.use(rootRouter);
 
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { rootController } from "../controllers/index";
 
 
+
 export const rootRouter = Router();
 
 rootRouter.use("/", rootController);
