55d3c0d4cb9995743e156d385d191dd8ca8bc822
Adel Sabanovic <62741023+Adel-Sabanovic@users.noreply.github.com>
2020-12-16 14:03:28 +0100

    Commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,8 +12,12 @@ config();
 
 const app = express();
 
-app.use(json());
+//app.use(json());
 
-app.use(rootRouter);
+//app.use(rootRouter);
+
+app.use("/", function(req, res){
+    res.send("Hej på dig mohammeeeeed")
+})
 
 app.listen(process.env.PORT, () => console.log(`server started at http://localhost:${process.env.PORT}/`));
