5a46fbb4c5158d5572c7ac26aedb084781f401aa
yousef <yousefkhalil125@gmail.com>
2021-01-11 23:01:19 +0100

    done

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,10 +1,14 @@
 const API_URL = "http://localhost:8000/api";
 
+/**
+ * @typedef {"p1" | "p2" | "din_gata"} Channel
+ */
+
 
 /**
  * Fetches from the server
  * 
- * @param {"p1" | "p2" | "din_gata"} channel
+ * @param {Channel} channel
  */
 async function fetchChannel(channel) {
 
@@ -13,6 +17,58 @@ async function fetchChannel(channel) {
     const response = await fetch(fetchUrl);
 
     const data = await response.json();
+
+    return data;
 }
 
-fetchChannel("din_gata");
+
+(async () => {
+    const channel = $("#page").text().trim();
+    
+    const body = await fetchChannel(channel);
+
+    const { 
+        success,
+        data: {
+            artist,
+            title,
+            url,
+            image
+        },
+        message
+    } = body
+
+    console.log(message);
+
+    $("#artist").text(artist);
+    
+    $("#title").text(title);
+
+    const btn = $("#btn");
+
+    const imageElementInStr = `
+        <div class="logo-btn-container">
+            <img class="logo-btn" src="spotify.svg">
+        </div>
+    `
+    .trim();
+
+    if (success) {
+    
+        $("#image").attr("src", image);
+    
+        btn.attr("href", url);
+
+        btn.text(`Add To Spotify`).append(imageElementInStr)
+    }
+    else {
+
+        btn.removeAttr("href");
+
+        btn.css("filter", "brightness(0.8)");
+
+        btn.css("cursor", "not-allowed");
+
+        btn.text(`Song Not Found`).append(imageElementInStr)
+    }
+})();
