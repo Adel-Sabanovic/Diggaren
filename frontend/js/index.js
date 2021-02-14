b10dbb6f0dd317a61dcff3c70368a2d158796eab
yousef <yousefkhalil125@gmail.com>
2021-02-14 12:37:13 +0100

    fixing frontend

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,99 +1,37 @@
 const API_URL = "http://localhost:8000/api";
 
-/**
- * @typedef {"p1" | "p2" | "din_gata"} Channel
- */
+setSelectorContent();
 
+async function setSelectorContent() {
 
-/**
- * Fetches from the server
- * 
- * @param {Channel} channel
- */
-async function fetchCurrentPlayingSong(channel) {
+    const response = await fetch(`${API_URL}/all-channels`);
 
-    const fetchUrl = `${API_URL}/channel/${channel}`;
+    const { data: channels } = await response.json();
 
-    const response = await fetch(fetchUrl);
+    const selector = document.getElementById("radio");
 
-    const json = await response.json();
+    selector.addEventListener("change", onSelectorChange);
 
-    return json;
+    for (const channel of channels) {
+
+        const option = document.createElement("option");
+
+        option.setAttribute("value", channel);
+
+        selector.appendChild(option)
+    }
 }
 
-async function fetchAllChannells() {
-
-    const fetchUrl = `${API_URL}/all-channels`;
-
-    const response = await fetch(fetchUrl);
-
-    const json = await response.json();
-
-    return json;
-}
-
-
-(async () => {
-    const channel = $("#page").text().trim();
+async function onSelectorChange(event) {
     
-    const body = await fetchCurrentPlayingSong(channel);
+    const response = await fetch(`${API_URL}/channel/${channel}`);
 
     const { 
         success,
-        data: {
-            artist,
-            title,
-            url,
-            image
-        },
         message,
-        httpStatusCode: {
-            status
-        }
-    } = body
+        data: song,
+        httpStatusCode
+    } = await response.json();
 
-    console.log(message);
-
-    $("#artist").text(artist);
-    
-    $("#title").text(title);
-
-    const btn = $("#btn");
-
-    const imageElementInStr = `
-        <div class="logo-btn-container">
-            <img class="logo-btn" src="spotify.svg">
-        </div>
-    `
-    .trim();
-
-    if (success) {
-    
-        $("#image").attr("src", image);
-    
-        btn.attr("href", url);
-
-        btn.text(`Add To Spotify`).append(imageElementInStr)
-    }
-    else {
-
-        if (status === "Gateway Time-out") {
-            btn.removeAttr("href");
-
-            btn.css("filter", "brightness(0.8)");
-    
-            btn.css("cursor", "not-allowed");
-    
-            btn.text(`Server is busy right now`).append(imageElementInStr);
-        }
-        else {
-            btn.removeAttr("href");
-
-            btn.css("filter", "brightness(0.8)");
-    
-            btn.css("cursor", "not-allowed");
-    
-            btn.text(`Song Not Found`).append(imageElementInStr)
-        }
-    }
-})();
+    const song = await fetchSong(event.target.value)
+}
